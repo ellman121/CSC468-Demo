8d72a5ba1d661fd8c89c4d9743e600e529fc0477
Elliott Rarden <ellman121@gmail.com>
2017-04-25 15:47:46 -0600

    Elements should be working now

--- Node_Backend/elements.js
+++ Node_Backend/elements.js
@@ -1,13 +1,32 @@
 var express = require('express');
+var fs = require('fs');
 
 var router = express.Router();
 
+// Prebuild things for fetching later
+
+function trimToSize(array) {
+	var slimArray = [];
+	for (elem in array) {
+		slimArray.push({
+			"symbol" : elem.symbol,
+			"name" : elem.name
+		})
+	}
+}
+
+var elementsArray = JSON.parse(fs.readFileSync('./elements.json'));
+var elementsSlim = trimToSize(elementsArray);
+
 router.get('/', (req, res, next) => {
-	// Return all emenents
+	res.send(slimArray);
 });
 
-router.get('/:element', (req, res, next) => {
-	// Return specified element
+router.get('/:number', (req, res, next) => {
+	var number = req.params.number;
+
+	res.send(elementsArray[number]);
+	// return elementsArray[number];
 });
 
 module.exports = router;
